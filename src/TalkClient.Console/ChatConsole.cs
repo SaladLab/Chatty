e74868c82222a1f70a5e13aae1537b3b8664ca8e
Esun Kim <veblush@gmail.com>
2016-05-30 17:23:49 +0900

    Cleanup console client.

--- src/TalkClient.Console/ChatConsole.cs
+++ src/TalkClient.Console/ChatConsole.cs
@@ -12,7 +12,9 @@ namespace TalkClient.Console
     {
         private Communicator _communicator;
         private UserRef _user;
-        private Dictionary<string, OccupantRef> _occupantMap = new Dictionary<string, OccupantRef>();
+        private UserEventObserver _userEventObserver;
+        private Dictionary<string, Tuple<OccupantRef, RoomObserver>> _roomMap =
+            new Dictionary<string, Tuple<OccupantRef, RoomObserver>>();
         private string _currentRoomName;
 
         public async Task RunAsync(Communicator communicator)
@@ -21,7 +23,7 @@ namespace TalkClient.Console
 
             WriteLine("[ ChatConsole ]");
 
-            _user = await LoginAsync();
+            await LoginAsync();
             WriteLine("# logined");
 
             await EnterRoomAsync("#general");
@@ -30,14 +32,15 @@ namespace TalkClient.Console
             await ChatLoopAsync();
         }
 
-        private async Task<UserRef> LoginAsync()
+        private async Task LoginAsync()
         {
             var userLogin = _communicator.CreateRef<UserLoginRef>();
             var observer = _communicator.CreateObserver<IUserEventObserver>(this);
 
             try
             {
-                return (UserRef)(await userLogin.Login("console", "1234", observer));
+                _user = (UserRef)(await userLogin.Login("console", "1234", observer));
+                _userEventObserver = (UserEventObserver)observer;
             }
             catch (Exception)
             {
@@ -48,7 +51,7 @@ namespace TalkClient.Console
 
         private async Task ChatLoopAsync()
         {
-            ShowCommandHelp();
+            OnCommandShowHelp();
             WriteLine("");
 
             while (true)
@@ -65,100 +68,35 @@ namespace TalkClient.Console
                         case "/enter":
                         case "/j":
                         case "/join":
-                            try
-                            {
-                                await EnterRoomAsync(words[1]);
-                            }
-                            catch (Exception e)
-                            {
-                                WriteLine("Failed to join: " + e);
-                            }
+                            await OnCommandEnterRoom(words);
                             break;
 
                         case "/x":
                         case "/exit":
                         case "/l":
                         case "/leave":
-                            try
-                            {
-                                var leaveRoom = words.Length > 1 ? words[1] : _currentRoomName;
-                                await ExitRoomAsync(leaveRoom);
-                                _occupantMap.Remove(leaveRoom);
-                                if (_occupantMap.ContainsKey(_currentRoomName) == false)
-                                    _currentRoomName = _occupantMap.Keys.FirstOrDefault();
-                            }
-                            catch (Exception e)
-                            {
-                                WriteLine("Failed to leave:" + e);
-                            }
+                            await OnCommandExitFromRoom(words);
                             break;
 
                         case "/c":
                         case "/current":
-                            if (words.Length > 1)
-                            {
-                                var roomName = words[1];
-                                if (_occupantMap.ContainsKey(roomName))
-                                {
-                                    _currentRoomName = roomName;
-                                    WriteLine($"Current room is changed to {_currentRoomName}");
-                                }
-                                else
-                                {
-                                    WriteLine($"No room");
-                                }
-                            }
-                            else
-                            {
-                                WriteLine($"Current room: {_currentRoomName}");
-                            }
+                            OnCommandCurrentChannel(words);
                             break;
 
                         case "/i":
                         case "/invite":
-                            if (string.IsNullOrEmpty(_currentRoomName))
-                            {
-                                WriteLine("Need a room to invite");
-                            }
-                            else
-                            {
-                                var occupant = _occupantMap[_currentRoomName];
-                                for (var i = 1; i < words.Length; i++)
-                                {
-                                    WriteLine("Invite: " + words[i]);
-                                    try
-                                    {
-                                        await occupant.Invite(words[i]);
-                                    }
-                                    catch (Exception e)
-                                    {
-                                        WriteLine("Failed to invite: " + e);
-                                    }
-                                }
-                            }
+                            await OnCommandInviteUser(words);
                             break;
 
                         case "/w":
                         case "/whisper":
-                            if (words.Length >= 3)
-                            {
-                                try
-                                {
-                                    var targetUser = words[1];
-                                    var message = string.Join(" ", words.Skip(2));
-                                    await _user.Whisper(targetUser, message);
-                                }
-                                catch (Exception e)
-                                {
-                                    WriteLine("Failed to whisper: " + e);
-                                }
-                            }
+                            await OnCommandWhisper(words);
                             break;
 
                         case "/?":
                         case "/h":
                         case "/help":
-                            ShowCommandHelp();
+                            OnCommandShowHelp();
                             break;
 
                         default:
@@ -174,18 +112,112 @@ namespace TalkClient.Console
                     }
                     else
                     {
-                        await _occupantMap[_currentRoomName].Say(line);
+                        await _roomMap[_currentRoomName].Item1.Say(line);
                     }
                 }
             }
         }
 
-        private void ShowCommandHelp()
+        private async Task OnCommandEnterRoom(string[] words)
+        {
+            try
+            {
+                await EnterRoomAsync(words[1]);
+            }
+            catch (Exception e)
+            {
+                WriteLine("Failed to join: " + e);
+            }
+        }
+
+        private async Task OnCommandExitFromRoom(string[] words)
+        {
+            try
+            {
+                var room = words.Length > 1 ? words[1] : _currentRoomName;
+                if (string.IsNullOrEmpty(room))
+                {
+                    WriteLine("No room to exit.");
+                    return;
+                }
+                await ExitRoomAsync(room);
+                if (_roomMap.ContainsKey(_currentRoomName) == false)
+                    _currentRoomName = _roomMap.Keys.FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                WriteLine("Failed to leave:" + e);
+            }
+        }
+
+        private void OnCommandCurrentChannel(string[] words)
+        {
+            if (words.Length > 1)
+            {
+                var roomName = words[1];
+                if (_roomMap.ContainsKey(roomName))
+                {
+                    _currentRoomName = roomName;
+                    WriteLine($"Current room is changed to {_currentRoomName}");
+                }
+                else
+                {
+                    WriteLine($"No room");
+                }
+            }
+            else
+            {
+                WriteLine($"Current room: {_currentRoomName}");
+            }
+        }
+
+        private async Task OnCommandInviteUser(string[] words)
+        {
+            if (string.IsNullOrEmpty(_currentRoomName))
+            {
+                WriteLine("Need a room to invite");
+            }
+            else
+            {
+                var occupant = _roomMap[_currentRoomName].Item1;
+                for (var i = 1; i < words.Length; i++)
+                {
+                    WriteLine("Invite: " + words[i]);
+                    try
+                    {
+                        await occupant.Invite(words[i]);
+                    }
+                    catch (Exception e)
+                    {
+                        WriteLine("Failed to invite: " + e);
+                    }
+                }
+            }
+        }
+
+        private async Task OnCommandWhisper(string[] words)
+        {
+            if (words.Length >= 3)
+            {
+                try
+                {
+                    var targetUser = words[1];
+                    var message = string.Join(" ", words.Skip(2));
+                    await _user.Whisper(targetUser, message);
+                }
+                catch (Exception e)
+                {
+                    WriteLine("Failed to whisper: " + e);
+                }
+            }
+        }
+
+        private void OnCommandShowHelp()
         {
             WriteLine("Commands:");
             WriteLine("  /e channel   Enter channel                   (alias: /enter /j /join)");
-            WriteLine("  /x [channel] Leave (current) channel         (alias: /exit /l /leave)");
-            WriteLine("  /c [channel] Show or change current channel  (alias: /current)");
+            WriteLine("  /x [channel] Exit from (current) channel     (alias: /exit /l /leave)");
+            WriteLine("  /c [channel] Show or set current channel     (alias: /current)");
             WriteLine("  /i user      Invite user                     (alias: /invite)");
             WriteLine("  /w user msg  Whisper to user                 (alias: /whisper)");
         }
@@ -212,7 +244,7 @@ namespace TalkClient.Console
             try
             {
                 var ret = await _user.EnterRoom(name, observer);
-                _occupantMap.Add(name, (OccupantRef)ret.Item1);
+                _roomMap.Add(name, Tuple.Create((OccupantRef)ret.Item1, (RoomObserver)observer));
                 _currentRoomName = name;
                 return ret.Item2;
             }
@@ -225,8 +257,13 @@ namespace TalkClient.Console
 
         private async Task ExitRoomAsync(string name)
         {
+            Tuple<OccupantRef, RoomObserver> item;
+            if (_roomMap.TryGetValue(name, out item) == false)
+                throw new Exception("Cannot find room: " + name);
+
             await _user.ExitFromRoom(name);
-            // TODO: Remove Observer
+            item.Item2.Dispose();
+            _roomMap.Remove(name);
         }
 
         void IUserEventObserver.Invite(string invitorUserId, string roomName)
