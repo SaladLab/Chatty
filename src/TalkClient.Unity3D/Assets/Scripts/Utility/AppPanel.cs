a8a95cf70bbc7326c287238796a8991c91e77046
Esun Kim <veblush@gmail.com>
2016-07-04 15:40:12 +0900

    Add version info to unity client.

---
+++ src/TalkClient.Unity3D/Assets/Scripts/Utility/AppPanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AppPanel : MonoBehaviour
+{
+    public Text VersionText;
+
+    private void Start()
+    {
+        var commitId = string.IsNullOrEmpty(Version.CommitId)
+                           ? ""
+                           : "(" + Version.CommitId.Substring(0, 6) + ")";
+        VersionText.text = "Ver: " + Version.VersionString + " " + commitId;
+    }
+}
---
+++ src/TalkClient.Unity3D/Assets/Scripts/Utility/UnityCloudBuildManifest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class UnityCloudBuildManifest
+{
+#pragma warning disable SA1307 // Accessible fields must begin with upper-case letter
+
+    public string scmCommitId;
+    public string scmBranch;
+    public string buildNumber;
+    public string buildStartTime;
+    public string projectId;
+    public string bundleId;
+    public string unityVersion;
+    public string xcodeVersion;
+    public string cloudBuildTargetName;
+
+ #pragma warning restore SA1307 // Accessible fields must begin with upper-case letter
+
+    public static UnityCloudBuildManifest Load()
+    {
+        var manifest = (TextAsset)Resources.Load("UnityCloudBuildManifest.json");
+        if (manifest == null)
+            return null;
+
+        return JsonUtility.FromJson<UnityCloudBuildManifest>(manifest.text);
+    }
+}
